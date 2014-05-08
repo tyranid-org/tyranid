73fadf86624253375da86ccb4bd02245ceceea30
Dimensia <ted@dimensiology.com>
2014-05-08 14:14:45 -0500

    initial version

---
+++ README.md
@@ -0,0 +1,2 @@
+MongoDB ODM library for node.js.
+
---
+++ tyranid.js
@@ -0,0 +1,73 @@
+
+var lodash = require( 'lodash' );
+
+/*
+   /. server inherits client objects
+
+   /. validation (server vs. client)
+
+   /. authorized methods + filtering attributes to the client
+
+   /. offline support
+
+   /. store cache on client
+
+   /. tids ?
+   
+      f23<mongo id (24 chars) >
+
+      <letter + 2 alphanum><mongo id>
+
+
+   ?. how do we share code between server and client from the same source .js file ?
+
+
+  var String = {
+  }
+
+  var Date = {
+  }
+
+
+
+  representations:
+
+  (a)
+
+  {
+    name: 'Event',
+    fields: {
+      name: { type: String },
+      desc: { type: String },
+      when: { type: Date   }
+    }
+  }
+
+  (b)
+
+  function Event({
+    ...
+  });
+
+*/
+
+function classify( proto, staticMethods ) {
+  var constructor = proto.constructor ? proto.constructor : function() {};
+  constructor.prototype = proto;
+  return _.extend( constructor, staticMethods );
+}
+
+var Collection = classify(
+  function( def ) {
+    this.def = def;
+  },
+  {
+  }
+);
+
+module.export = {
+
+  Collection: Collection
+
+};
+
