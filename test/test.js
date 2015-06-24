b4001c8ff4ae15c6423feb9917f06f0dc9f7861c
shindig <markb@dimensiology.com>
2015-06-24 08:57:45 -0500

    added another friend for nested array test failure

--- test/test.js
+++ test/test.js
@@ -151,7 +151,7 @@ describe( 'tyranid', function() {
           return Person.db.insert([
             { _id: 1, organization: 1, department: 1, name: { first: 'An', last: 'Anon' }, title: 'Developer' },
             { _id: 2, organization: 1, name: { first: 'John', last: 'Doe' }, homepage: 'https://www.tyranid.org' },
-            { _id: 3, organization: 2, name: { first: 'Jane', last: 'Doe' }, siblings: [ { name: 'Jill Doe', friends: [ { person : 1 } ] } ] }
+            { _id: 3, organization: 2, name: { first: 'Jane', last: 'Doe' }, siblings: [ { name: 'Jill Doe', friends: [ { person : 1, person: 2 } ] }, { name: 'Bill Doe', friends: [ { person : 2 } ] } ] }
           ]);
         }),
         Task.db.remove({}).then(function() {
@@ -231,7 +231,7 @@ describe( 'tyranid', function() {
     });
 
     describe('values', function() {
-      var allString = [ '123 Construction', 'Acme Unlimited', 'An', 'Anon', 'Developer', 'Doe', 'Engineering', 'Jane', 'Jill Doe', 'John' ];
+      var allString = [ '123 Construction', 'Acme Unlimited', 'An', 'Anon', 'Bill Doe', 'Developer', 'Doe', 'Engineering', 'Jane', 'Jill Doe', 'John' ];
 
       it( 'should support valuesFor()', function() {
         Person.valuesFor(Person.fieldsBy({ name: 'string' })).then(function(values) {
@@ -303,6 +303,8 @@ describe( 'tyranid', function() {
           .then(function(people) {
             return Person.populate([ 'organization', 'siblings.friends.person' ], people).then(function(people) {
               expect(people[2].siblings[0].friends[0].person$._id).to.be.eql(1);
+              expect(people[2].siblings[0].friends[1].person$._id).to.be.eql(2);
+              expect(people[2].siblings[1].friends[0].person$._id).to.be.eql(2);
             });
           });
       });
