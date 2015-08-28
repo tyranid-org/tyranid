ac4706c8133021f1331f0fa7c61a217eb8345740
Ben Southgate <bsouthga@gmail.com>
2015-08-28 10:40:14 -0400

    formatting + dependency updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,58 @@
-
+'use strict';
 var gulp       = require('gulp'),
-  eslint     = require('gulp-eslint'),
-  mocha      = require('gulp-mocha'),
-  sourcemaps = require('gulp-sourcemaps'),
-  babel      = require('gulp-babel'),
-  yargs      = require('yargs');
+    eslint     = require('gulp-eslint'),
+    mocha      = require('gulp-mocha'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel      = require('gulp-babel'),
+    yargs      = require('yargs');
 
-gulp.task('compile', ['eslint', 'test'], function() {
+var src = './src/**/*.js',
+    test = './test/**/*.js';
+
+/*
+ * ES6/7 -> ES5 for npm publishing
+ */
+gulp.task('compile', function() {
   return gulp
-    .src('src/**/*.js')
+    .src([src])
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./dist/'));
 });
 
+
+/*
+ * Babel-assisted linter
+ */
 gulp.task('eslint', function() {
   return gulp
-    .src(['src/**/*.js', 'test/**/*.js'])
+    .src([src, test])
     .pipe(eslint('.eslintrc'))
     .pipe(eslint.format());
 });
 
+
+/*
+ * Linting watch task for development
+ */
 gulp.task('watch', function() {
-  gulp.watch(['src/**/*.js', 'test/**/*.js'], ['eslint']);
+  return gulp.watch([src, test], ['eslint']);
 });
 
-gulp.task('test', function(cb) {
-  gulp.src(['test/**/*.js','!test/models/**'], {
-    read: false
-  })
-  .pipe(mocha({
-    reporter: 'spec',
-    require : ['babel/register'],
-    grep: yargs.argv.grep
-  }))
-  .on('end', function() {
-    cb(null);
-  });
+
+/*
+ * Mocha tests
+ */
+gulp.task('test', function() {
+  return gulp
+    .src([test,'!test/models/**'], { read: false })
+    .pipe(mocha({
+      reporter: 'spec',
+      // babel import hook
+      require : ['babel/register'],
+      grep: yargs.argv.grep
+    }));
 });
 
 gulp.task('default', ['eslint']);
