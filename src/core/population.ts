319dc773112ba28b5a41998ba4d44d18c321d387
Ben Southgate <bsouthga@gmail.com>
2016-04-18 01:16:56 -0400

    convert two more

--- src/core/population.ts
+++ src/core/population.ts
@@ -1,16 +1,14 @@
 
 import * as _ from 'lodash';
-
 import Tyr      from '../tyr';
 import NamePath from './namePath';
-import { ObjectId } from 'mongodb';
+import { ObjectID as ObjectId } from 'mongodb';
 
 const $all = Tyr.$all;
 
 export default class Population {
 
-
-  constructor(namePath, projection) {
+  constructor(public namePath: NamePath, public projection: any) {
     if (!(namePath instanceof NamePath)) {
       throw new Error('parameter namePath is not an instanceof NamePath, got: ' + namePath);
     }
