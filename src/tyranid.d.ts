ec1b31d9f650eddae33799824f2d97651ec81e19
Dimensia <ted@dimensiology.com>
2017-12-23 17:32:30 -0600

    add isSameId()

--- src/tyranid.d.ts
+++ src/tyranid.d.ts
@@ -66,7 +66,7 @@ declare namespace Tyranid {
      * utility methods
      */
     export function isEqual(a: any, b: any): boolean;
-    export function isSameId(a: string | IdType, b: string | IdType): boolean;
+    export function isSameId(a: string | IdType | null | undefined, b: string | IdType | null | undefined): boolean;
     export function indexOf(arr: any[], item: any): number;
     export function addToSet(set: any[], item: any): void;
     export function pullAll(arr: any[], item: any): void;
