e5e039ab3ae01a7436b22541f4c8c870a4254e7e
Ben Southgate <bsouthga@gmail.com>
2018-04-02 14:03:37 -0400

    feat: add style check to travis

---
+++ scripts/style-check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "running prettier..."
+OUTPUT=$(npm run prettier)
+CHANGED=$(git diff-index --name-only HEAD --)
+if [ -n "$CHANGED" ]; then
+  echo "non standard formatting found, please run prettier locally (npm run prettier)..."
+  echo "the following files have changed after formatting: "
+  echo "$CHANGED"
+  exit 1;
+else
+  echo "formatting correct!"
+  exit 0;
+fi
+
+echo "running linter..."
+npm run lint --silent
+
