010d76cc896ccc0336e3396a9b44cbf83697878e
Ben Southgate <bsouthga@gmail.com>
2018-04-02 14:13:56 -0400

    fix: try to exclude bin from prettier check

--- scripts/style-check.sh
+++ scripts/style-check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "running prettier..."
 OUTPUT=$(npm run prettier)
-CHANGED=$(git diff-index --name-only HEAD --)
+CHANGED=$(git diff-index --name-only HEAD -- . ':(exclude)packages/tyranid-tdgen/bin/tyranid-tdgen')
 if [ -n "$CHANGED" ]; then
   echo "non standard formatting found, please run prettier locally (npm run prettier)..."
   echo "the following files have changed after formatting: "
