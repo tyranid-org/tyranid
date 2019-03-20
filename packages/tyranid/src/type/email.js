9d5c738b6d6c62ba05d47851e8c0e33cfe9b467e
Dimensia <ted@dimensiology.com>
2019-03-20 09:57:26 -0500

    fix email field defn

--- packages/tyranid/src/type/email.js
+++ packages/tyranid/src/type/email.js
@@ -1,5 +1,37 @@
 import Type from '../core/type';
 
-const EmailType = new Type({ name: 'email' });
+const EmailType = new Type({
+  name: 'email',
+  compare(field, a, b) {
+    return a.localeCompare(b, undefined, { sensitivity: 'base' });
+  },
+
+  query(namePath, where, query) {
+    if (where) {
+      query[namePath.name] = _.isArray(where) ? { $in: where } : where;
+    }
+  },
+
+  matches(namePath, where, doc) {
+    if (where) {
+      let value = namePath.get(doc);
+      if (value) {
+        if (!_.isString(value)) {
+          value = '' + value;
+        }
+
+        if (_.isArray(where)) {
+          return where.indexOf(value) >= 0;
+        } else if (where instanceof RegExp) {
+          return value.match(where);
+        } else {
+          return value === where;
+        }
+      }
+    }
+
+    return true;
+  }
+});
 
 export default EmailType;
