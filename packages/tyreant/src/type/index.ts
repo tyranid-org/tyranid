1667edb11fdf137d7f34489cfd857bb8b936ed33
Dimensia <ted@dimensiology.com>
2019-08-23 10:19:23 -0500

    add integer support to tyreant

--- packages/tyreant/src/type/index.ts
+++ packages/tyreant/src/type/index.ts
@@ -4,6 +4,7 @@ export * from './array';
 export * from './date';
 export * from './datetime';
 export * from './email';
+export * from './integer';
 export * from './link';
 export * from './string';
 export * from './time';
