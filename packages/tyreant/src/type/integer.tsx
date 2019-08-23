7a899157b28be2e7060ed75196bb188fd29fa4bf
Dimensia <ted@dimensiology.com>
2019-08-23 10:20:31 -0500

    add integer support to tyreant

---
+++ packages/tyreant/src/type/integer.tsx
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import { useEffect } from 'react';
+
+import { Input } from 'antd';
+
+import { mapDocumentToForm } from './type';
+
+import {
+  byName,
+  className,
+  generateRules,
+  TyrTypeProps,
+  withTypeContext
+} from './type';
+
+export const TyrIntegerBase = ((props: TyrTypeProps) => {
+  const { document, field, form } = props;
+
+  useEffect(() => {
+    mapDocumentToForm(field, document, form);
+  });
+
+  return form!.getFieldDecorator(field.path, {
+    rules: generateRules(field)
+  })(
+    <Input className={className('tyr-integer', props)} step="1" type="number" />
+  );
+}) as React.ComponentType<TyrTypeProps>;
+
+export const TyrInteger = withTypeContext(TyrIntegerBase);
+/*
+export const integerFilter: Filter = (
+  field: Tyr.FieldInstance,
+  filterable: Filterable
+) => {
+  const { namePath } = field;
+  const pathName = field.path;
+  let searchInputRef: Input | null = null;
+
+  const onClearFilters = () => {
+    filterable.searchValues[pathName] = '';
+    filterable.onSearch();
+  };
+
+  filterable.searchValues[pathName] = filterable.searchValues[pathName] || '';
+
+  return {
+    filterDropdown: (
+      <div className="searchBox">
+        <Input
+          ref={node => {
+            searchInputRef = node;
+          }}
+          placeholder={`Search ${field.label}`}
+          value={filterable.searchValues[pathName]}
+          onChange={e => {
+            filterable.searchValues[pathName] = e.target.value;
+            filterable.onFilterChange();
+          }}
+          onPressEnter={() => filterable.onSearch()}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => filterable.onSearch()}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => onClearFilters()}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: (
+      <span>
+        <Icon
+          type="search"
+          className={filterable.searchValues[pathName] ? 'active' : 'inactive'}
+        />
+      </span>
+    ),
+    onFilter: (value: string, doc: Tyr.Document) =>
+      namePath
+        .get(doc)
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible: boolean) => {
+      if (visible) {
+        setTimeout(() => searchInputRef!.focus());
+      }
+    }
+  };
+};
+*/
+
+/*
+export const stringFinder: Finder = (
+  field: Tyr.FieldInstance,
+  opts: Tyr.anny, // Tyr.Options_Find,
+  searchValue: Tyr.anny
+) => {
+  if (searchValue) {
+    if (!opts.query) opts.query = {};
+    opts.query[field.path] = {
+      $regex: searchValue,
+      $options: 'i'
+    };
+  }
+};
+*/
+
+byName.integer = {
+  component: TyrIntegerBase
+  //filter: integerFilter,
+  //finder: integerFinder
+};
