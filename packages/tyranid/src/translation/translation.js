daee5ad7280e0bcec3de28e5be6e00747d7081de
Dimensia <ted@dimensiology.com>
2020-01-29 13:01:03 -0600

    initial design for translation system

---
+++ packages/tyranid/src/translation/translation.js
@@ -0,0 +1,76 @@
+
+import Tyr from '../tyr';
+
+import Collection from './collection';
+
+const Translation = new Collection({
+  id: '_t4',
+  name: 'tyrTranslation',
+  client: false,
+  internal: true,
+  timestamps: true,
+  fields: {
+    _id: { is: 'string' },
+    locale: { is: 'string' },
+  }
+});
+
+const Term = new Collection({
+  id: '_tm',
+  name: 'tyrTerm',
+  client: false,
+  internal: true,
+  timestamps: true,
+  fields: {
+    _id: { is: 'string' },
+    locale: { is: 'string' },
+    lastAliveOn: { is: 'date' }
+  }
+});
+
+Get label for question:
+
+Translation code format:
+
+UID_path
+
+Where path is <name>[.<property>]
+
+METADATA
+
+u00_name (implies .label)
+u00_name.help
+
+DATA
+
+t00question1_label
+
+STRINGS/TERMS
+
+Labels: ‘Welcome to $$MY_TERM’
+
+tyrTerm {
+  _id: ‘string’;
+  value: {
+    is: ‘string’,
+    help: ‘The default English value’
+  }
+}
+
+{ _id: ‘plan’, value: ‘Plan’ }
+
+tyrTransation {
+  Uid: ‘tt0plan’, { ....
+
+
+
+translations.findOne({
+Uid: Questions.uid,
+Code: PROFILE_GENDER_ID,
+Locale: ‘en’ 
+});
+
+Tyr.Translation = Translation;
+Tyr.Translation = Translation;
+
+export default Translation;
