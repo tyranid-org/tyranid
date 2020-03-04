3c79b096db89584c7fe49fd2dced4a2f3ca89daf
Dimensia <ted@dimensiology.com>
2020-03-03 19:49:30 -0600

    work on kanban

--- packages/tyreant/src/core/kanban.tsx
+++ packages/tyreant/src/core/kanban.tsx
@@ -17,22 +17,33 @@ import { TyrFilters } from './filter';
 import { TyrComponentState } from './component';
 import { TyrManyComponentProps, TyrManyComponent } from './many-component';
 
-interface ColumnDef {
-  label: string;
-  match: object;
-}
-
 interface ColumnData<D extends Tyr.Document> {
   id: string;
-  def: ColumnDef;
+  def: TyrKanbanColumnDef;
   cards: D[];
+}
+
+// this value doesn't really matter since we just care about significant digits,
+// some number larger than 0 just chosen for readability when debugging
+const MAX_INTERVAL = 1e12;
+
+export interface TyrKanbanColumnDef {
+  label: string;
+  match: object;
+
+  /**
+   * Any documents not matching any of the given match values will be placed into this column
+   * if default is set to true.
+   */
+  default?: boolean;
 }
 
 export interface TyrKanbanProps<D extends Tyr.Document>
   extends TyrManyComponentProps<D> {
   //across?: string;
   collection: Tyr.CollectionInstance<D>;
-  columns: ColumnDef[];
+  columns: TyrKanbanColumnDef[];
+  ordering: string;
   cardRenderer?: (document: D) => JSX.Element;
 }
 
@@ -59,64 +70,109 @@ export class TyrKanban<
   }
 
   async postFind() {
-    this.columns = this.props.columns.map((column, index) => {
-      return {
-        id: String(index),
-        def: column,
-        cards: this.documents.filter(doc => Tyr.isCompliant(column.match, doc))
-      };
-    });
+    let { documents } = this;
+    const { ordering, columns } = this.props;
+
+    if (ordering) {
+      const orderingPath = this.collection.parsePath(ordering);
+
+      // if no existing ordering values, initialize them based on current order
+      if (!documents.some(d => !!orderingPath.get(d))) {
+        const multiple = MAX_INTERVAL / documents.length;
+        let v = 0;
+
+        for (const d of documents) {
+          orderingPath.set(d, (v += multiple));
+          // TODO:  implement bulk api in tyranid and use that
+          await d.$update({ [ordering]: 1 });
+        }
+      } else {
+        documents = this.documents = documents
+          .slice()
+          .sort((a, b) => orderingPath.get(a) - orderingPath.get(b));
+      }
+    }
+
+    const tColumns = columns.map((column, index) => ({
+      id: String(index),
+      def: column,
+      cards: [] as D[] // new Array(this.columns / 2) ?
+    }));
+
+    const defIdx = columns.findIndex(c => c.default);
+    const def = defIdx >= 0 ? tColumns[defIdx] : undefined;
+
+    OUTER: for (const d of documents) {
+      for (const c of tColumns) {
+        if (Tyr.isCompliant(c.def.match, d)) {
+          c.cards.push(d);
+          continue OUTER;
+        }
+      }
+
+      if (def) {
+        def.cards.push(d);
+        const { match } = def.def;
+        Object.assign(d, match);
+        // TODO:  implement bulk api in tyranid and use that
+        await d.$update({ fields: Tyr.projectify(match) });
+      }
+    }
+
+    this.columns = tColumns;
   }
 
   onDragEnd = async (result: DropResult, provided: ResponderProvided) => {
     const { documents, columns } = this;
     const { destination, source, draggableId } = result;
 
-    if (!destination) {
-      return;
-    }
-
     if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) {
+      !destination ||
+      (destination.droppableId === source.droppableId &&
+        destination.index === source.index)
+    )
       return;
-    }
 
     const droppedCard = documents.find(d => d.$id === draggableId);
-    if (!droppedCard) {
-      console.log('Dropped card not found!');
-      return;
-    }
+    if (!droppedCard) return;
 
     const startColumn = columns.find(c => c.id === source.droppableId);
-    if (!startColumn) {
-      console.log('Droppable start column not found!');
-      return;
-    }
+    if (!startColumn) return;
 
     const finishColumn = columns.find(c => c.id === destination.droppableId);
-    if (!finishColumn) {
-      console.log('Droppable finish column not found!');
-      return;
-    }
+    if (!finishColumn) return;
+
+    const { ordering } = this.props;
+    const orderingPath = this.collection.parsePath(ordering);
+    const projection = { [ordering]: 1 };
+    const { cards } = finishColumn;
+    const dIdx = destination.index;
 
     if (startColumn === finishColumn) {
-      const { cards } = startColumn;
       cards.splice(source.index, 1);
-      cards.splice(destination.index, 0, droppedCard);
-      return;
+      cards.splice(dIdx, 0, droppedCard);
+    } else {
+      const startCards = startColumn.cards;
+      startCards.splice(source.index, 1);
+
+      cards.splice(dIdx, 0, droppedCard);
+
+      const { match } = finishColumn.def;
+      Object.assign(droppedCard, match);
+      Object.assign(projection, Tyr.projectify(match));
     }
 
-    const startCards = startColumn.cards;
-    startCards.splice(source.index, 1);
+    const prevOrd = dIdx > 0 ? orderingPath.get(cards[dIdx - 1]) : 0;
 
-    const finishCards = finishColumn.cards;
-    finishCards.splice(destination.index, 0, droppedCard);
+    const clen = cards.length;
+    const nextOrd =
+      dIdx < clen - 1 ? orderingPath.get(cards[dIdx + 1]) : MAX_INTERVAL;
 
-    const { match } = finishColumn.def;
-    Object.assign(droppedCard, match);
-    await droppedCard.$update({ fields: Tyr.projectify(match) });
+    //console.log({ clen, dIdx, prevOrd, nextOrd });
+
+    orderingPath.set(droppedCard, (prevOrd + nextOrd) / 2);
+
+    await droppedCard.$update({ projection });
   };
 
   render() {
@@ -152,7 +208,11 @@ export interface TyrKanbanColumnProps<D extends Tyr.Document> {
 }
 
 export const TyrKanbanColumn = observer(
-  <D extends Tyr.Document>({ column, cards }: TyrKanbanColumnProps<D>) => (
+  <D extends Tyr.Document>({
+    column,
+    cards,
+    cardRenderer
+  }: TyrKanbanColumnProps<D>) => (
     <div className="tyr-kanban-column">
       <div className="tyr-kanban-column-title">{column.def.label}</div>
       <Droppable droppableId={column.id}>
@@ -165,7 +225,12 @@ export const TyrKanbanColumn = observer(
             }`}
           >
             {cards.map((card, index) => (
-              <TyrCard key={card.$id} document={card} index={index} />
+              <TyrCard
+                key={card.$id}
+                document={card}
+                index={index}
+                cardRenderer={cardRenderer}
+              />
             ))}
             {provided.placeholder}
           </div>
