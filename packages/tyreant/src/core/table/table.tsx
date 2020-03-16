19575843a2df8070f0d2cbf356baf00025bdcb25
Mark Bradley <markb@dimensiology.com>
2020-03-16 16:33:37 -0500

    filter work

--- packages/tyreant/src/core/table/table.tsx
+++ packages/tyreant/src/core/table/table.tsx
@@ -4,7 +4,6 @@
    - store in tableConfig
  - store filters in tableConfig
  - store sort in tableConfig
- 
 */
 
 import { compact, findIndex, isEqual } from 'lodash';
@@ -631,7 +630,7 @@ export class TyrTableBase<
           const render = column.renderDisplay;
 
           return (
-            <div className='tyr-table-cell'>
+            <div className="tyr-table-cell">
               {render
                 ? render(document)
                 : getCellValue(path!, document, column as TyrTypeProps)}
@@ -701,7 +700,7 @@ export class TyrTableBase<
         }`,
         title: !newDocumentTable
           ? actionHeaderLabel || (
-              <Tooltip title='Edit Columns'>
+              <Tooltip title="Edit Columns">
                 <MenuOutlined />
               </Tooltip>
             )
@@ -715,15 +714,15 @@ export class TyrTableBase<
 
           if (editable) {
             if (this.isSavingDocument) {
-              return <Spin tip='Saving...' />;
+              return <Spin tip="Saving..." />;
             }
 
             return (
               <div style={{ display: 'flex' }}>
                 <Button
                   style={{ marginRight: 8, zIndex: 1 }}
-                  size='small'
-                  type='default'
+                  size="small"
+                  type="default"
                   onClick={() => this.cancelEdit()}
                 >
                   Cancel
@@ -739,9 +738,9 @@ export class TyrTableBase<
 
                     return (
                       <Button
-                        size='small'
+                        size="small"
                         style={{ zIndex: 1 }}
-                        type='primary'
+                        type="primary"
                         onClick={() => this.saveDocument(form)}
                       >
                         Save
@@ -773,7 +772,7 @@ export class TyrTableBase<
             if (typeof label === 'string') {
               return (
                 <a
-                  className='action-item'
+                  className="action-item"
                   onClick={e => {
                     e.preventDefault();
                     e.stopPropagation();
@@ -787,7 +786,7 @@ export class TyrTableBase<
 
             return (
               <span
-                className='action-item'
+                className="action-item"
                 onClick={e => {
                   e.preventDefault();
                   e.stopPropagation();
@@ -800,9 +799,9 @@ export class TyrTableBase<
           }
 
           const menu = (
-            <Menu className='tyr-menu'>
+            <Menu className="tyr-menu">
               {thisActions.map(action => (
-                <Menu.Item className='tyr-menu-item' key={action.name}>
+                <Menu.Item className="tyr-menu-item" key={action.name}>
                   <button
                     onClick={() => action.act({ caller: this, document })}
                   >
@@ -815,7 +814,7 @@ export class TyrTableBase<
 
           return (
             <Dropdown overlay={menu} trigger={[actionTrigger || 'click']}>
-              <span className='tyr-menu-link'>
+              <span className="tyr-menu-link">
                 {actionIcon || <EllipsisOutlined />}
               </span>
             </Dropdown>
@@ -1090,7 +1089,7 @@ export class TyrTableBase<
         >
           {(children || multiActions.length > 0 || voidActions.length > 0) && (
             <Row>
-              <Col span={24} className='tyr-table-header'>
+              <Col span={24} className="tyr-table-header">
                 {children}
                 {multiActions.map(a => (
                   <Button
@@ -1124,7 +1123,7 @@ export class TyrTableBase<
               {paths && newDocument && (
                 <ObsTable
                   bordered={bordered}
-                  className='tyr-table-new-document'
+                  className="tyr-table-new-document"
                   loading={loading}
                   components={components}
                   rowKey={() => 'new'}
