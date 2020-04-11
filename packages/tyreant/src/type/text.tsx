46e73fa6090241975d965e3528787295d1194ad9
Mark Bradley <markb@dimensiology.com>
2020-04-11 17:51:58 -0500

    ckeditor for large text

--- packages/tyreant/src/type/text.tsx
+++ packages/tyreant/src/type/text.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import { useEffect } from 'react';
 
-import { Input } from 'antd';
-
 import { Tyr } from 'tyranid/client';
 
 import { byName, TyrTypeProps, mapPropsToForm, onTypeChange } from './type';
@@ -11,23 +9,54 @@ import { withThemedTypeContext } from '../core/theme';
 import { decorateField } from '../core';
 import { registerComponent } from '../common';
 
-const { TextArea } = Input;
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+const CKEditorWrapper = <D extends Tyr.Document = Tyr.Document>({
+  value = {},
+  onChange,
+  props,
+}: {
+  value?: any;
+  onChange?: any;
+  props: TyrTypeProps<D>;
+}) => {
+  const [editor, setEditor] = React.useState<any>();
+
+  React.useEffect(() => {
+    if (editor) editor.setData(value);
+  }, [editor]);
+
+  return (
+    <CKEditor
+      editor={ClassicEditor}
+      onInit={(editor: any) => {
+        setEditor(editor);
+      }}
+      config={{
+        placeholder: props.placeholder,
+      }}
+      onChange={(event: any, editor: any) => {
+        const data = editor.getData();
+        onTypeChange(props, data, event);
+        onChange({ ...value, ...data });
+      }}
+      onBlur={(event: any, editor: any) => {
+        //console.log('Blur.', editor);
+      }}
+      onFocus={(event: any, editor: any) => {
+        //console.log('Focus.', editor);
+      }}
+    />
+  );
+};
 
 export const TyrTextBase = <D extends Tyr.Document = Tyr.Document>(
   props: TyrTypeProps<D>
 ) => {
   useEffect(() => mapPropsToForm(props), [props.path?.name]);
 
-  return decorateField('text', props, () => (
-    <TextArea
-      placeholder={props.placeholder}
-      autoFocus={props.autoFocus}
-      onChange={ev => onTypeChange(props, ev.target.value, ev)}
-      tabIndex={props.tabIndex}
-      rows={props.textAreaRows || 6}
-      onPressEnter={props.onPressEnter}
-    />
-  ));
+  return decorateField('text', props, () => <CKEditorWrapper props={props} />);
 };
 
 export const TyrText = withThemedTypeContext('text', TyrTextBase);
@@ -39,3 +68,16 @@ byName.text = {
 };
 
 registerComponent('TyrText', TyrText);
+/*
+import { Input } from 'antd';
+const { TextArea } = Input;
+
+<TextArea
+placeholder={props.placeholder}
+autoFocus={props.autoFocus}
+onChange={ev => onTypeChange(props, ev.target.value, ev)}
+tabIndex={props.tabIndex}
+rows={props.textAreaRows || 6}
+onPressEnter={props.onPressEnter}
+/>
+*/
