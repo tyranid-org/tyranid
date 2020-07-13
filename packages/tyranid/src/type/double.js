932e74fec628f900ba435d5d2649e33f3b9c1762
Mark Bradley <markb@dimensiology.com>
2020-07-13 10:11:56 -0500

    fix double parsing

--- packages/tyranid/src/type/double.js
+++ packages/tyranid/src/type/double.js
@@ -32,7 +32,7 @@ const DoubleType = new Type({
 
       const v = parseFloat(value);
 
-      if (v.toString() !== value) {
+      if (isNaN(v)) {
         throw new Error(`Invalid double on field ${field.name}: ${value}`);
       }
 
