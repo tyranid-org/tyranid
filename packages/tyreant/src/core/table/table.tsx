198efc1c675c8b591b8dd894049532508590568b
Mark Bradley <markb@dimensiology.com>
2020-11-18 07:28:56 -0600

    tweak for publish

--- packages/tyreant/src/core/table/table.tsx
+++ packages/tyreant/src/core/table/table.tsx
@@ -1021,7 +1021,7 @@ export class TyrTableBase<
           row: EditableFormRow as any,
           /*
           CELL_EDIT
-          cell: EditableCell,
+          cell: EditableCell
           */
         },
       };
